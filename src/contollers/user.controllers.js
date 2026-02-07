f8e7aef5032d476dcc417289f4bd31172dd7963d
Lucky Jha <jhalucky61@gmail.com>
2026-02-07 22:28:33 +0530

    file added

--- src/contollers/user.controllers.js
+++ src/contollers/user.controllers.js
@@ -1,9 +1,25 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/apiError.js"
+import { User } from "../models/user.models.js"
 
 const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        message: "chup chaap register user route"
-    })
+   const {fullName, username, email, password} = req.body;
+
+   console.log("email: ", email)
+
+   if ([fullName, username, email, password].some((field) => 
+    field?.trim === "")) 
+   {
+    throw new ApiError(400, "fullname is required")
+   } 
+
+   const existedUser = User.findOne({
+    $or : [{username}, {email}]
+   })
+
+   if (existedUser) {
+    throw new ApiError(409, "User with this username or email already exists")
+   }
 })
 
 export {registerUser}
